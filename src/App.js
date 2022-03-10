97245b90d91b48358f7928f2dbb2dbfbbed31a1d
olgien <olgakanclerz@gmail.com>
2022-03-10 17:06:24 +0100

    Database, scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Strona główna</Link>
+            </li>
+            <li>
+              <Link to="/about">Mój sprzęt</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Udostępnij listę sprzętu</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+          <Switch>
+            <Route exact path="/">
+              lorem ipsum
+            </Route>
+            <Route path="/about">
+              Mój sprzęt:
+            </Route>
+            <Route path="/dashboard">
+              Udostępnij swoją listę sprzętu:
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
